f87b288ed807d2c6b05afd1b18c631dc6dea5bd8
Londerson Araújo <londerson@gmail.com>
2023-12-23 11:49:41 -0300

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_23_133728) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_23_144548) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,4 +43,15 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_23_133728) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "viewed_campgrounds", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "campground_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["campground_id"], name: "index_viewed_campgrounds_on_campground_id"
+    t.index ["user_id"], name: "index_viewed_campgrounds_on_user_id"
+  end
+
+  add_foreign_key "viewed_campgrounds", "campgrounds"
+  add_foreign_key "viewed_campgrounds", "users"
 end
