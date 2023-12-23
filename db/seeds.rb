0227919ebbde0dd97642cd0b7268e9f044a1aedf
Londerson Araújo <londerson@gmail.com>
2023-12-23 10:31:25 -0300

    Setup seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,139 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+campgrounds_data =
+[
+  {
+    "name": "Lake Perris Campground",
+    "overview": "<p>Ready for a wildlife and adventure filled camping weekend? Lake Perris camping has 167 sites for tent camping and 264 paved RV sites (which have water, electricity, and sink water hookups). There is one hike and bike campsite located by the horseshoe pit, and there are 7 primitive horse camps. The sites are all equipped with a table, fire ring with a grill, and access to piped water. Most plants and animals at Lake Perris are well adapted to the hot, dry environment, so make sure you’re prepared as well. It can get quite warm in the summer and there isn’t too much tree coverage – so bring a canopy if you have one! Or just stay in the lake the whole time… that’s fine too.</p>",
+    "state": "California",
+    "city": "Moreno Valley",
+    "cover_photo_url": "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1433546047/p5g8bgxrs7heihxo0sus.jpg",
+    "contributed_photos": [
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1433541573/tmpqfi270lurojesnsqs.jpg",
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1433541574/fhzbtfrkn9tsoncbkb2t.jpg",
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1433541575/varech6sjxqr4b0nqefy.jpg",
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1433541576/yyyafqiald2pjytowhs6.jpg",
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1433546047/p5g8bgxrs7heihxo0sus.jpg"
+    ]
+  },
+  {
+    "name": "Roaring River Campground",
+    "overview": "There’s a lot to rave about at Roaring River Campground, foremost being the unimaginably breathtaking views of deep green growth on gentle hills that tower over blue spring waters. If you want a good-hearted family-bonding sort of experience, undisturbed by the busy world outside of this pristine encapsulation of natural beauty, then pack your kids and fishing poles into the truck and get yourselves over here. We’re sure you’ll love and appreciate your time at this clean (and uncommercialized) part of Roaring River State Park. Did we mention that the campsites are a two minute walk from the river? And that the waters are stocked each night by one of our nation’s premier trout fisheries? Yes, awesome.",
+    "state": "Missouri",
+    "city": "Eagle Rock",
+    "cover_photo_url": "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1471918359/campground-photos/k4xpocqd6guouuihdnj8.jpg",
+    "contributed_photos": [
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1438053727/pbltdgjzdg2koe4onavw.jpg",
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1438053728/dylbngqp0bokbsqjkjjb.jpg",
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1438053729/grf7xi7pi6nhvhlprezj.jpg",
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1438053730/vgsd91zixhuybwy6rwpj.jpg",
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1471918359/campground-photos/k4xpocqd6guouuihdnj8.jpg"
+    ]
+  },
+  {
+    "name": "Arroyo Campground",
+    "overview": "Shaded with sycamore and oak trees, Arroyo campground is like a forested wonderland right near the park’s entrance and accessible from the road. It has 79 individual campsites, all in relatively close proximity to one another. They’ve got it all here: potable water, grills, picnic tables, ground stoves, barbeques, flush toilets, a dumping ground, and even free hot showers! Pawfoot Trail runs alongside the campground for easy access to some awesome hiking, horseback riding, or biking. \r\n\r\n\r\nIf your group is larger than 17 people, be sure to reserve a group campsite in either Hickey Creek or Sycamore Flats group area, which offer 7 group sites total. (Note: Group campsite D in Sycamore Flats is pretty close to the creek, if that’s your thing.) Bringing your horse along? Hermon Equestrian Camping area offers 5 different sites, all relatively close to the creek with site 1 being the most private.\r\n",
+    "state": "California",
+    "city": "Rancho Santa Margarita",
+    "cover_photo_url": "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1494976811/campground-photos/p6qrmkj9ztnqtdgpyd0o.jpg",
+    "contributed_photos": [
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1433541604/zx3yzjmggzwv462shom2.jpg",
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1433541605/kuqvk42de4uklycsjzaz.jpg",
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1433541606/jxqflyg4q3a3z5giwbhm.jpg",
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1433541607/xxqxauhnb781qjy2qn6n.jpg",
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1494976811/campground-photos/p6qrmkj9ztnqtdgpyd0o.jpg"
+    ]
+  },
+  {
+    "name": "Lower Lehman Creek Campground",
+    "overview": "If you’re camping in the Great Basin, you should give Lower Lehman Creek Campground a shot. Campsites are right on the creek, and it’s just a few miles away from Lehman Caves on the Wheeler Peak scenic drive. Look forward to exploring geologic wonders, cooking by the campfire, and staring up at that river of stars in the sky!",
+    "state": "Nevada",
+    "city": "Baker",
+    "cover_photo_url": "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1490022828/campground-photos/qxljxawszhpzttu3evnd.jpg",
+    "contributed_photos": [
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1490022828/campground-photos/qxljxawszhpzttu3evnd.jpg",
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1490022829/campground-photos/pqazrv3se01n5enhjzdz.jpg",
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1490022830/campground-photos/uxvbd7vhlej43acqru29.jpg",
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1493873232/campground-photos/d4rbtb29cnxmzkj1dsf8.jpg",
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1493873234/campground-photos/jx3tfxtkjgsjnpaws3fk.jpg"
+    ]
+  },
+  {
+    "name": "Big Meadows Campground",
+    "overview": "We hope you have an impressive bike rack on your car, because you'll want to pack as much gear as possible for all the adventure available at Big Meadows Campground. Float around Big Meadows Reservoir on a tube or canoe, fish for rainbow and brook trout in the area or nearby streams, or hike and bike around one of several trails. Did we mention you can go by horseback as well? Don't forget to bring that dorky sunhat of yours, because shade is limited. Come winter, snowmobiling and XC skiing dominate.",
+    "state": "Colorado",
+    "city": "Alpine",
+    "cover_photo_url": "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1439394445/campground-photos/svi852gwlwngqz283j6q.jpg",
+    "contributed_photos": [
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1439394209/campground-photos/bcgam216yuhilukhfykd.jpg",
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1439394445/campground-photos/svi852gwlwngqz283j6q.jpg",
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1439394448/campground-photos/gnju2n6ourab0n7ru4sb.jpg",
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1439394450/campground-photos/mvcazvydu6qldxolz4c2.jpg",
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1439394453/campground-photos/txqqueixscs00lduvcme.jpg"
+    ]
+  },
+  {
+    "name": "Lumber River Campground",
+    "overview": "Lumber River is one of America’s few remaining wild and scenic rivers which makes it a must-see destination. Why not spend a few days here and camp at one of two designated primitive camping areas within Lumber River State Park? From there you can plan a small boat or canoe trip, and really get up close and personal with this marvelous waterway. Bring your rod and catch some dinner from the water and then settle in for a sleep under the stars. Don’t let life lumber past you, get out there and make some memories in the wild.",
+    "state": "North Carolina",
+    "city": "Boardman",
+    "cover_photo_url": "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1434730211/xdbrchyfyoich9fmkt3g.jpg",
+    "contributed_photos": [
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1434730209/lu0n21xbno1t684xjja8.jpg",
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1434730210/s2wdcqoihlfktgewd5ut.jpg",
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1434730211/xdbrchyfyoich9fmkt3g.jpg"
+    ]
+  },
+  {
+    "name": "Mather Campground",
+    "overview": "Storybook forest camping at the Grand Canyon, who knew? Mather Campground provides spacious and shady RV, tent, and even horse sites with stars at night that are simply unbeatable. There’s great sites for group camping, and your party may just turn into a group with all the new elk pals you’re bound to make. Water, super clean restrooms, a dump station, and showers means you don’t have to get completely grizzly on your adventures, and the free park shuttle is a short walk to take you right to the rim. Mather is the place to kick it on the South Rim, just ask the elk!",
+    "state": "Arizona",
+    "city": "Grand Canyon Village",
+    "cover_photo_url": "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1516904017/campground-photos/qor4n0xusaldno9w1zgm.jpg",
+    "contributed_photos": [
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1437599485/campground-photos/bfwdtjfhxvrxus6di5ma.jpg",
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1437599578/campground-photos/vzdjyhtkuyb5xnsuburv.jpg",
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1437599583/campground-photos/zllskubgkqyd4lkgyqkn.jpg",
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1437599592/campground-photos/myvedljxzwoxvk7j6z60.jpg",
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1437599600/campground-photos/dflio9shcj8rcvbuuqcx.jpg"
+    ]
+  },
+  {
+    "name": "Wheeler Gorge Campground",
+    "overview": "Yep… this is the one that you’re not supposed to tell anyone about. Perhaps, one of Southern California’s best kept secrets, Wheeler Gorge Campground contains numerous sites adjacent to a picturesque stream and rocky mountains. Perfect for tent camping, each site contains a table, bbq, and fire ring. There is no dump station or portable water on site, so come prepared. Great base to explore Matilija Creek, the town of Ojai, or the surrounding Wilderness Areas. Once you’re snug in your hammock, swaying by the stream, give a shout out to the Boy Scouts who built the campground nearly a century ago!",
+    "state": "California",
+    "city": "Mission Canyon",
+    "cover_photo_url": "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1445881896/campground-photos/t6xkwu1ijamumdfrpjup.jpg",
+    "contributed_photos": [
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1433546056/p2ahwpihjaj7acprlqns.jpg",
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1445881890/campground-photos/qd7l6g6zn5qwrsktxxzf.jpg",
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1445881892/campground-photos/ezytgmcctajevpycn3fx.jpg",
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1445881893/campground-photos/wmsqyvmuofcomjkruhmt.jpg",
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1445881896/campground-photos/t6xkwu1ijamumdfrpjup.jpg"
+    ]
+  },
+  {
+    "name": "Cape Perpetua Campground",
+    "overview": "From steep cliffs to rolling hills, and soft sand dunes to verdant forests, Cape Perpetua Campground has got access to every blissful nature escape you could possibly need, plus some. Such as tide pools, which, just, yes. Drinking water, flush toilets, picnic tables, and campfire rings complete this dreamy locale, which spans wide enough to host 37 tent, auto, and trailer sites, including 1 groupe site, and 37 RV sites. Take a long hike or chill right at your temporary home for your whole stay. Either way, you’re guaranteed a rejuvenating time. Here’s the <a href=\"http://www.fs.usda.gov/recarea/siuslaw/recreation/recarea/?recid=42273&amp;actid=29\" target=\"_blank\" rel=\"nofollow\">campground’s website</a> so you can get a handle on when this sweet place is open to campers!",
+    "state": "Oregon",
+    "city": "Yachats",
+    "cover_photo_url": "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1430097775/campground-photos/tvlw1lbddoithfcvrxmd.jpg",
+    "contributed_photos": [
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1430097775/campground-photos/tvlw1lbddoithfcvrxmd.jpg",
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1432156074/campground-photos/xmdo3hpcfm7q8bjmqo7f.jpg",
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1432156099/campground-photos/nxfq6ccbikmgvpnl3ztn.jpg",
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1432393181/campground-photos/bb4uprxles7otjnva9ga.jpg",
+      "https://img.hipcamp.com/image/upload/c_fill,f_auto,g_auto,h_900,q_60,w_2040/v1435270288/campground-photos/ygrz67wsrkhfdno6l2pm.jpg"
+    ]
+  }
+]
+
+campgrounds_data.each do |camp|
+  campground = Campground.create!(name: camp[:name], overview: camp[:overview], city: camp[:city], cover_photo_url: camp[:cover_photo_url])
+
+  camp[:contributed_photos].each do |url|
+    contributed_photo = ContributedPhoto.create!(url: url, campground_id: campground.id)
+    campground.contributed_photos.push(contributed_photo)
+    campground.save
+  end
+end
+
+puts 'Seeds successfully executed.'
