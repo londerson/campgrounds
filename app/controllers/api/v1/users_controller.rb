d32481edeb8916c428cd288711ce3a019dd20420
Londerson Araújo <londerson@gmail.com>
2023-12-23 12:49:26 -0300

    Change nomenclatures

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :fetch_user, only: [:show]
+  before_action :fetch_user, only: [:recently_viewed]
 
-  def show
+  def recently_viewed
     @top_recently_viewed_campgrounds = Campground.find(@user.top_recently_viewed_campgrounds).as_json(only: [:id, :name, :overview, :city, :cover_photo_url])
     render json: { top_recently_viewed_campgrounds: @top_recently_viewed_campgrounds }
   end
@@ -9,6 +9,6 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def fetch_user
-    @user = User.find(params[:id])
+    @user = User.find(params[:user_id])
   end
 end
